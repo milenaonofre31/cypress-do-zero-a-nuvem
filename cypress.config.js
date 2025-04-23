4089947ea498d3f2f10b94289f555af71ddf8380
MILENA DIANNY GONDIM ONOFRE <monofre@ad.trt13.jus.br>
2025-04-23 10:56:56 -0300

    configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({ //alterando as dimensões de altura e largura padrão do cypress
+  projectId: "8zx7px",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {}, //projeto end-to-end
   //video:true
+  
 });
